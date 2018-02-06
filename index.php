dc2f6f305064c2138956515606370c3b627164b5
phil <phil@home.de>
2018-02-06 18:35:37 +0100

    Added some css and google fonts

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	
 </head>
@@ -18,8 +19,8 @@
 
 if (isset($_POST["submit"])) {
 
-	$email = $_POST["email"];
-	$pw = password_hash($_POST["pw"], PASSWORD_DEFAULT);
+		$email = $_POST["email"];
+		$pw = password_hash($_POST["pw"], PASSWORD_DEFAULT);
 
 	echo "Mail: " . $email . "</br>" . "Password: " . $pw;
 
