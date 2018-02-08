92d99de9d5850423a8f6ea4742d090d69a17fa29
phil <phil@home.de>
2018-02-08 18:37:23 +0100

    Created a completely new interface with a navbar

--- main.php
+++ main.php
@@ -10,18 +10,57 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     
 </head>
-<body>
 
-<div class="card bg-dark mx-auto">
-	<div class="card-header text-center text-light">Beautiful Login</div>
-	<div class="card-body mx-auto">
-		<form action="index.php" method="post">
-			<input type="email" name="email" placeholder="E-Mail">
-			<input type="password" name="pw" placeholder="Password">
-			<button class="btn-light" type="submit" name="submit">Login</button>
-		</form>
-	</div>
-</div>
+<body id="main">
+
+	<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
+        <div class="container">
+        	
+         	 <a class="navbar-brand" href="#">Navbar</a>
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Services</a>
+                  </li class="nav-item">
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Contact</a>
+                  </li>
+              </ul>
+          </div>
+    </nav>
+
+    <div class="container">
+    	
+		 <div class="card">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Disabled</a>
+                </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Beautiful Login</h4>
+                <form action="index.php" method="post">
+				<input type="email" name="email" placeholder="E-Mail">
+				<input type="password" name="pw" placeholder="Password">
+				<button href="#" class="btn btn-primary" type="submit" name="submit">Login</button>
+				</form>
+            </div>
+        </div>
+    	
+    </div>
 
 </body>
 </html>
